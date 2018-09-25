3f38bf98aa160f77baa232ac053669fbc45e99eb
Zerovers <Zeromon19941994@mail.ru>
2018-09-25 19:21:11 +0300

    emplemented getZerosCount

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+    let a, b, c, d;
+    let zeroCount = 0;
+    a = base;
+
+    for (let i = 2; i <= base; i++) {
+        if (a % i == 0) {
+            b = 0;
+            while (a % i == 0) {
+                b++;
+                a = parseInt(a / i);
+
+            }
+            c = 0;
+            d = number;
+            while (parseInt(d / i) > 0) {
+                c = (parseInt(d / i)) + c;
+                d = parseInt(d / i);
+            }
+            zeroCount = Math.min(number, parseInt(c / b));
+        }
+    }
+
+
+    return zeroCount;
 }
